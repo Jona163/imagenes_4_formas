ac545df9828defeb4775efdd7bf35e707d3c841b
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-17 20:23:25 -0600

    Update 4_formas.py

--- 4_formas.py
+++ 4_formas.py
@@ -2,3 +2,13 @@
 # Fecha: 18 Septiembre 2024
 # Descripción: Código para procesamiento de imagenes con Sobel 20 formas.
 # GitHub: https://github.com/Jona163
+
+# Cargar la imagen
+image = Image.open('chems1.jpg')
+image = np.array(image)
+
+# Función para mostrar la imagen
+def plot_image(image, title, ax):
+    ax.imshow(image)
+    ax.set_title(title, fontsize=10)
+    ax.axis('off')
