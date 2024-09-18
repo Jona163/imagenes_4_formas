8635905e13023b0eb5dc1d61b94a64707274eb90
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-17 20:26:56 -0600

    Update 4_formas.py

--- 4_formas.py
+++ 4_formas.py
@@ -1,7 +1,12 @@
 # Autor: Jonathan Hernández
 # Fecha: 18 Septiembre 2024
-# Descripción: Código para procesamiento de imagenes con Sobel 20 formas.
+# Descripción: Código para procesamiento de imagenes 4 formas.
 # GitHub: https://github.com/Jona163
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from scipy.ndimage import affine_transform
 
 # Cargar la imagen
 image = Image.open('chems1.jpg')
@@ -13,7 +18,6 @@ def plot_image(image, title, ax):
     ax.set_title(title, fontsize=10)
     ax.axis('off')
 
-
 # Crear matrices de transformación
 # 1. Traslación
 def translation_matrix(tx, ty):
@@ -21,13 +25,11 @@ def translation_matrix(tx, ty):
                      [0, 1, ty],
                      [0, 0, 1]])
 
-
 # 2. Rotación
 def rotation_matrix(theta):
     return np.array([[np.cos(theta), -np.sin(theta), 0],
                      [np.sin(theta), np.cos(theta), 0],
                      [0, 0, 1]])
-
 
 # 3. Escalamiento
 def scaling_matrix(sx, sy):
@@ -35,13 +37,11 @@ def scaling_matrix(sx, sy):
                      [0, sy, 0],
                      [0, 0, 1]])
 
-
 # 4. Cizallado
 def shearing_matrix(kx, ky):
     return np.array([[1, kx, 0],
                      [ky, 1, 0],
                      [0, 0, 1]])
-
 
 # Función para aplicar la transformación
 def apply_transformation(image, matrix):
@@ -51,18 +51,18 @@ def apply_transformation(image, matrix):
             inverse_matrix = np.linalg.inv(matrix)[:2, :2]
             offset = np.linalg.inv(matrix)[:2, 2]
             transformed_image[..., i] = affine_transform(image[..., i], inverse_matrix, offset=offset, output_shape=image.shape[:2])
-            else:  # Imágenes en escala de grises
+    else:  # Imágenes en escala de grises
         inverse_matrix = np.linalg.inv(matrix)[:2, :2]
         offset = np.linalg.inv(matrix)[:2, 2]
         transformed_image = affine_transform(image, inverse_matrix, offset=offset, output_shape=image.shape)
-     return transformed_image
+    
+    return transformed_image
 
 # Configurar las subplots
 fig, axs = plt.subplots(2, 2, figsize=(10, 10))
 
 # Título general para el conjunto de imágenes
 fig.suptitle('Transformaciones Imágenes', fontsize=16)
-
 
 # Aplicar las transformaciones
 # 1. Traslación (50 píxeles en x y 30 en y)
@@ -75,7 +75,6 @@ rotation = rotation_matrix(np.radians(45))
 rotated_image = apply_transformation(image, rotation)
 plot_image(rotated_image, 'Rotación 45°', axs[0, 1])
 
-
 # 3. Escalamiento (1.5x en X y 0.5x en Y)
 scaling = scaling_matrix(1.5, 0.5)
 scaled_image = apply_transformation(image, scaling)
@@ -86,8 +85,6 @@ shearing = shearing_matrix(0.5, 0.2)
 sheared_image = apply_transformation(image, shearing)
 plot_image(sheared_image, 'Cizallado (0.5 en X, 0.2 en Y)', axs[1, 1])
 
-
 # Ajustar el layout para evitar solapamientos
 plt.tight_layout(rect=[0, 0, 1, 0.95])
 plt.show()
-
